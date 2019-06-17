56c4938b878b09f88e42cce824c670eee96d72b6
Jaris <jaris@liminalvr.com>
2019-06-17 10:35:11 +1000

    Add menu item for creating Readme files easily (Assets/Create/Readme.md | directly below C# Script)

---
+++ Editor/MenuItems.cs
@@ -0,0 +1,29 @@
+﻿#if UNITY_EDITOR
+using System.IO;
+
+using UnityEditor;
+using UnityEngine;
+
+internal static class MenuItems
+{
+    [MenuItem("Assets/Create/Readme.md", false, 81)]
+    private static void CreateReadme()
+    {
+        var obj = Selection.activeObject;
+        var dir = obj == null ? "Assets" : AssetDatabase.GetAssetPath(obj.GetInstanceID());
+        int number = 0;
+        string baseName = "Readme";
+        string fileName = baseName;
+        while (File.Exists(Path.Combine(dir, $"{fileName}.md")))
+        {
+            number++;
+            fileName = $"{baseName}{number:00}";
+        }
+        var path = Path.Combine(dir, $"{fileName}.md");
+        File.Create(path).Close();
+        AssetDatabase.Refresh();
+        System.Diagnostics.Process.Start(path);
+        Debug.Log($"Created Readme file at: {path}");
+    }
+}
+#endif
