277359c5abe9395d6a10f0f3e20587a2949f4930
Jaris <jaris@liminalvr.com>
2019-08-02 15:07:58 +1000

    Add utility to make sprite from tex.

--- Core/Utility.cs
+++ Core/Utility.cs
@@ -16,6 +16,11 @@
     {
         private static readonly Random _rand = new Random(DateTime.Now.Millisecond);
 
+        public static Sprite ToSprite(this Texture2D texture)
+        {
+            return Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0.5f));
+        }
+
         /// <summary>
         /// Make an enumerable of all integers between min and max
         /// </summary>
